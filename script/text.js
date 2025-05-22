5ddc089a901ec4d6521fdc0b6452a6fb33b0ca0b
Ikotofotsy <nyrinaratovo@gmail.com>
2025-05-22 13:41:37 +0000

    Mise a jour

--- script/text.js
+++ script/text.js
@@ -1,10 +1,3 @@
 document.addEventListener("DOMContentLoaded", (event) =>{
-    gsap.to('.title h1', {
-        duration: 2,
-        text: {
-            value : "RATOVOARISON Tsiory Ny Rina"
-        },
-
-        ease: "none",
-    });
+    
 });
